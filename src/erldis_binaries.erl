6eeb59a5a319107267d2ca49a80d6bc8b791ab65
Jacob Perkins <japerk@gmail.com>
2010-05-22 20:43:05 -0700

    simple binary key encoding util functions

--- src/erldis_binaries.erl
+++ src/erldis_binaries.erl
@@ -1,6 +1,6 @@
 -module(erldis_binaries).
 
--export([to_binary/1, join/2]).
+-export([to_binary/1, join/2, encode_key/1, encode_key_parts/1]).
 
 to_binary(X) when is_list(X) -> list_to_binary(X);
 to_binary(X) when is_atom(X) -> list_to_binary(atom_to_list(X));
@@ -11,6 +11,8 @@ to_binary(X) -> term_to_binary(X).
 
 join([], _)->
 	<<>>;
+join(Array, Sep) when not is_binary(Sep) ->
+	join(Array, to_binary(Sep));
 join(Array, Sep)->
 	F = fun(Elem, Acc) ->
 			E2 = to_binary(Elem),
@@ -20,3 +22,7 @@ join(Array, Sep)->
 	Sz = size(Sep),
 	<<_:Sz/bytes, Result/binary>> = lists:foldl(F, <<>>, Array),
 	Result.
+
+encode_key(Key) -> re:replace(Key, <<" ">>, <<"_">>, [{return, binary}]).
+
+encode_key_parts(Parts) -> encode_key(join(Parts, <<":">>)).
